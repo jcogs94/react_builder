60b16c7a14045958cc287228ed88f1afb137460a
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-07-17 11:28:52 -0400

    naming scheme updated

--- data/get_app_jsx_lines.py
+++ data/get_app_jsx_lines.py
@@ -4,7 +4,7 @@ def get_app_jsx_lines(base_was_init):
     if base_was_init:
         app_jsx_lines.extend([
             "import Nav from './components/Nav/Nav.jsx'\n",
-            "import MainComponent from './components/MainContainer/MainContainer.jsx'\n",
+            "import MainContainer from './components/MainContainer/MainContainer.jsx'\n",
             "import Footer from './components/Footer/Footer.jsx'\n",
         ])
 
