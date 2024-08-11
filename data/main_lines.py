0fa73f5d3fcd19bc789ef345b4485ab45bab7d20
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-08-11 12:42:07 -0400

    adjusted main_lines for react-router-dom

--- data/main_lines.py
+++ data/main_lines.py
@@ -1,10 +1,13 @@
 main_lines = [
     "import React from 'react'\n",
     "import ReactDOM from 'react-dom/client'\n",
+    "import { BrowserRouter } from 'react-router-dom'\n",
     "import App from './App.jsx'\n\n",
     "ReactDOM.createRoot(document.getElementById('root')).render(\n",
     "  <React.StrictMode>\n",
-    "    <App />\n",
+    "    <BrowserRouter>\n",
+    "      <App />\n",
+    "    </BrowserRouter>\n",
     "  </React.StrictMode>,\n",
     ")\n"
 ]
